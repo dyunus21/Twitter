f1256f96da7fb62fb9f7aa0cf5aeaca681ab775d
Diya Yunus <diyayunus@fb.com>
2022-06-06 15:32:05 -0700

    Added Tweet and User Models

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,20 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    public String name;
+    public String screenName;
+    public String publicImageUrl;
+
+    // Unpack data from Json Object
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.publicImageUrl = jsonObject.getString("public_image_url");
+        return user;
+    }
+}
